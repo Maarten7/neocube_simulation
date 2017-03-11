792ccdbfdd7ce501f10f9a9a99480a350cec356f
Maarten7 <maarten7post@gmail.com>
2017-03-11 17:37:30 +0100

    Create simulation.cpp
    Kleine startings

---
+++ simulation.cpp
@@ -0,0 +1,19 @@
+// Maarten & David
+
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <stdlib.h>
+#include <cstring>
+#include <cstdlib>
+#include <math.h>
+#include <cmath>
+
+mu = mu_0/(4*PI) ;
+	
+
+double dipool(double x, double y, bouble z)
+{
+	r = sqrt(pow(x,2) + pow(y,2) + pow(z,2));
+	rhat = x/r , y/r z/r;
+}
